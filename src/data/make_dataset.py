db808814786ff9bbc3afbc22feb386f5a7b45315
Julio Zampier <juliozampier@gmail.com>
2022-12-14 12:43:12 -0300

    Import each *.csv file and create a dataframe

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,18 +3,44 @@
 import pandas as pd
 import pathlib
 
-
-path_db = pathlib.Path('../../data/raw')
-base_airbnb = pd.DataFrame()
+months: dict = {
+    'jan': 1,
+    'fev': 2,
+    'mar': 3,
+    'abr': 4,
+    'mai': 5,
+    'jun': 6,
+    'jul': 7,
+    'ago': 8,
+    'set': 9,
+    'out': 10,
+    'nov': 11,
+    'dez': 12,
+}
+path_db: str = pathlib.Path('../../data/raw')
+base_airbnb: pd.DataFrame = pd.DataFrame()
 
 
 for file in path_db.iterdir():
+
+    month_name: str = file.name[:3]
+    month: str = months.get(month_name)
+
+    year: str = file.name[-8:]
+    year: int = int(year.replace('.csv', ''))
+
+    df = pd.read_csv(path_db / file.name)
+    df['ano'] = year
+    df['mes'] = month
+
+    base_airbnb = pd.concat([base_airbnb, df], axis=0)
     print(file.name)
 
-abril2018_df = pd.read_csv(r'../../data/raw/abril2018.csv')
 #! Consolidate the dataset
+base_airbnb
 
 #! Exclude unwanted columns
+# *9
 
 #! Adjust Nan values
 
