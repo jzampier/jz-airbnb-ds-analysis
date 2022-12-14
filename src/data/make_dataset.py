826bb41653fb58807eaffc1e31152558a4451f05
Julio Zampier <juliozampier@gmail.com>
2022-12-14 09:44:39 -0300

    Updade make_dataset.py code

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,15 +1,25 @@
 """Script to create our dataset from csv files"""
-# Import Library and Data Bases
+#! Import Library and Data Bases
 import pandas as pd
+import pathlib
 
-# Consolidate the dataset
 
-# Exclude unwanted columns
+path_db = pathlib.Path('../../data/raw')
+base_airbnb = pd.DataFrame()
 
-# Adjust Nan values
 
-# Verify data types for each column
+for file in path_db.iterdir():
+    print(file.name)
 
-# Exploratory analysis treat Outliers
+abril2018_df = pd.read_csv(r'../../data/raw/abril2018.csv')
+#! Consolidate the dataset
 
-# Encoding
+#! Exclude unwanted columns
+
+#! Adjust Nan values
+
+#! Verify data types for each column
+
+#! Exploratory analysis treat Outliers
+
+#! Encoding
