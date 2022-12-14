ba2f1179a7d28111e6dff0edeaf72e39b53c8dc7
Julio Zampier <juliozampier@gmail.com>
2022-12-14 01:09:13 -0300

    Include some folders to structure

---
+++ src/data/make_dataset.py
@@ -0,0 +1,15 @@
+"""Script to create our dataset from csv files"""
+# Import Library and Data Bases
+import pandas as pd
+
+# Consolidate the dataset
+
+# Exclude unwanted columns
+
+# Adjust Nan values
+
+# Verify data types for each column
+
+# Exploratory analysis treat Outliers
+
+# Encoding
